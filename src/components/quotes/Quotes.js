bc282086f536849371865b55d5b195fe602936c3
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-21 20:53:51 +0200

    Add uuid to generate unique id for qoute

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuotes } from '../../redux/reducers/quotes';
 import Quote from './Quote';
@@ -12,7 +13,7 @@ const Quotes = () => {
   const quotes = useSelector((state) => state.quotes);
 
   const allQoutes = quotes.map((quote) => (
-    <Quote key={quote.qoute_id} quoteID={quote.quoteID} quoteValue={quote.quoteValue} />
+    <Quote key={uuidv4()} quoteID={quote.quoteID} quoteValue={quote.quoteValue} />
   ));
 
   return (
