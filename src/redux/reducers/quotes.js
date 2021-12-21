6b59d04e17fe4c12eb0a73843ede20c06cdc32ae
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-21 19:34:27 +0200

    Add fetch quotes reducers

--- src/redux/reducers/quotes.js
+++ src/redux/reducers/quotes.js
@@ -1,19 +1,33 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 const FETCH_QUOTES = 'fetch/trump/quotes';
+const fetchApi = 'https://www.tronalddump.io/search/quote?query=women';
 
-export const fetchQuotes = (payload) => ({
-  type: FETCH_QUOTES,
-  payload,
-});
+export const fetchQuotes = () => async (dispatch) => {
+  const res = await axios.get(fetchApi);
+  // eslint-disable-next-line no-underscore-dangle
+  const result = res.data._embedded.quotes;
+  const fetchedQoutes = [];
+  Object.entries(result).forEach(([key, value]) => {
+    const quotes = {
+      index: key,
+      quoteID: value.quote_id,
+      quoteValue: value.value,
+    };
+    fetchedQoutes.push(quotes);
+  });
+  // console.log(result);
+  // console.log(fetchedQoutes);
+  dispatch({
+    type: FETCH_QUOTES,
+    payload: fetchedQoutes,
+  });
+};
 
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_QUOTES:
-      return {
-        ...state,
-        ...action.payload,
-      };
+      return [...action.payload];
     default:
       return state;
   }
