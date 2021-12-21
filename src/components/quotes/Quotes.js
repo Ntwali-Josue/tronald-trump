b683d149fbefa06f25a78aa5ae4d878703f42e00
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-22 00:40:57 +0200

    Add back button to qoutes component

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchQuotes } from '../../redux/reducers/quotes';
 import Quote from './Quote';
@@ -8,7 +9,7 @@ const Quotes = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchQuotes());
-  }, []);
+  }, [100000]);
 
   const quotes = useSelector((state) => state.quotes);
 
@@ -17,7 +18,10 @@ const Quotes = () => {
   ));
 
   return (
-    <div>
+    <div className="details-container">
+      <NavLink className="back-btn" to="/">
+        <img alt="Back button" src="https://img.icons8.com/ios-glyphs/30/ffffff/circled-left-2.png" />
+      </NavLink>
       {allQoutes}
     </div>
   );
