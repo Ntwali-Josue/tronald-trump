0052b7e34b6f8d1159933bd917c72063e0d9b403
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-21 21:19:36 +0200

    Render Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
 import Quotes from './quotes/Quotes';
+import NavBar from './NavBar';
 
 const App = () => (
-  <div className="App">
-    <Quotes />
+  <div>
+    <Routers>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Quotes />} />
+      </Routes>
+    </Routers>
   </div>
 );
 
