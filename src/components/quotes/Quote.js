ac72fe03b0568992037612348e7fe3363651a99e
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-21 19:46:09 +0200

    Add quotes folder

---
+++ src/components/quotes/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Quote = ({ quoteValue }) => (
+  <div>
+    <p>{quoteValue}</p>
+  </div>
+);
+Quote.propTypes = {
+  // quoteID: PropTypes.string.isRequired,
+  quoteValue: PropTypes.string.isRequired,
+};
+
+export default Quote;
---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchQuotes } from '../../redux/reducers/quotes';
+import Quote from './Quote';
+
+const Quotes = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchQuotes());
+  }, []);
+
+  const quotes = useSelector((state) => state.quotes);
+
+  const allQoutes = quotes.map((quote) => (
+    <Quote key={quote.qoute_id} quoteID={quote.quoteID} quoteValue={quote.quoteValue} />
+  ));
+
+  return (
+    <div>
+      yooooooo
+      {allQoutes}
+    </div>
+  );
+};
+
+export default Quotes;
