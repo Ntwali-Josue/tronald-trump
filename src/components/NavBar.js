777b09b09900e9ea080f75d5d9e5048a9da51f44
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-21 23:28:45 +0200

    Add classes to navbar components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => (
     <header className="nav-header">
       <img alt="Mobile menu button" src="https://img.icons8.com/material-rounded/24/ffffff/menu--v1.png" />
       <NavLink to="/">
-        <h1 className="nav-title">Trump</h1>
+        <h1 className="nav-title">Trump Qoutes</h1>
       </NavLink>
       <div className="nav-icon">
         <img alt="config button" src="https://img.icons8.com/external-those-icons-fill-those-icons/24/ffffff/external-gear-settings-those-icons-fill-those-icons-1.png" />
