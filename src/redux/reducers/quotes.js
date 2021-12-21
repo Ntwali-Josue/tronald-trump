5a83e2a0b1efd1542cfad78b08e370909923b16f
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-22 00:41:40 +0200

    Add try and catch methods to handle errors

--- src/redux/reducers/quotes.js
+++ src/redux/reducers/quotes.js
@@ -1,31 +1,43 @@
 import axios from 'axios';
 
-const topics = (topic) => topic;
 const FETCH_QUOTES = 'fetch/trump/quotes';
-const fetchApi = `https://www.tronalddump.io/search/quote?query=${topics('money')}`;
+const PICK_TOPIC = 'pick/trump/topics';
+
+export const topics = (topic) => ({
+  type: PICK_TOPIC,
+  topic,
+});
+
+const fetchApi = `https://www.tronalddump.io/search/quote?query=${topics('women')}`;
 
 export const fetchQuotes = () => async (dispatch) => {
-  const res = await axios.get(fetchApi);
-  const result = res.data._embedded.quotes;
-  const fetchedQoutes = [];
-  Object.entries(result).forEach(([key, value]) => {
-    const quotes = {
-      index: key,
-      quoteID: value.quote_id,
-      quoteValue: value.value,
-    };
-    fetchedQoutes.push(quotes);
-  });
-  dispatch({
-    type: FETCH_QUOTES,
-    payload: fetchedQoutes,
-  });
+  try {
+    const res = await axios.get(fetchApi);
+    const result = res.data._embedded.quotes;
+    const fetchedQoutes = [];
+    Object.entries(result).forEach(([key, value]) => {
+      const quotes = {
+        index: key,
+        quoteID: value.quote_id,
+        quoteValue: value.value,
+      };
+      fetchedQoutes.push(quotes);
+    });
+    dispatch({
+      type: FETCH_QUOTES,
+      payload: fetchedQoutes,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const reducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_QUOTES:
       return [...action.payload];
+    case PICK_TOPIC:
+      return state;
     default:
       return state;
   }
