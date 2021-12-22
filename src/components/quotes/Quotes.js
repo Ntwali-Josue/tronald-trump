870cfef32d2b39a772b2d597641acab02010744e
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-22 15:36:34 +0200

    Remove uncessary dispatched actions

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchQuotes } from '../../redux/reducers/quotes';
+import { useSelector } from 'react-redux';
+// import { fetchQuotes } from '../../redux/reducers/quotes';
 import Quote from './Quote';
 
 const Quotes = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchQuotes());
-  }, [100000]);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchQuotes());
+  // }, []);
 
-  const quotes = useSelector((state) => state.quotes);
-
+  const quotes = useSelector((state) => state.quotes.fetchedQoutes);
+  console.log(quotes);
   const allQoutes = quotes.map((quote) => (
     <Quote key={uuidv4()} quoteID={quote.quoteID} quoteValue={quote.quoteValue} />
   ));
