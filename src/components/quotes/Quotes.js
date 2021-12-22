07e40b5231ba6627fdcfada491d687db5e076a08
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-22 15:52:34 +0200

    Remove comments and uncessary imports in Qoutes component

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -2,17 +2,10 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-// import { fetchQuotes } from '../../redux/reducers/quotes';
 import Quote from './Quote';
 
 const Quotes = () => {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchQuotes());
-  // }, []);
-
   const quotes = useSelector((state) => state.quotes.fetchedQoutes);
-  console.log(quotes);
   const allQoutes = quotes.map((quote) => (
     <Quote key={uuidv4()} quoteID={quote.quoteID} quoteValue={quote.quoteValue} />
   ));
