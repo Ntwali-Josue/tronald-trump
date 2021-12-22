681c2240ba13a529c31b3b6626ca19b52198d9c9
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-22 20:12:09 +0200

    Render images on each topic card

--- src/components/topics/Topic.js
+++ src/components/topics/Topic.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Topic = ({ topicName }) => (
+const Topic = ({ image, topicName }) => (
   <>
     <li className="topic-item">
       <div>
-        <h1>{topicName}</h1>
+        <img src={image} className="image" alt="images" />
+        <h1 className="topic-title">
+          {topicName}
+          <img alt="Front button" src="https://img.icons8.com/ios-glyphs/30/ffffff/circled-right-2.png" />
+        </h1>
       </div>
     </li>
   </>
 );
 
 Topic.propTypes = {
+  image: PropTypes.string.isRequired,
   topicName: PropTypes.string.isRequired,
 };
 
