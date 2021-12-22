3f3691b58796bdb3f1c69cee69692f001267df9f
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-23 01:26:31 +0200

    Ädd count and total Qoutes states in reducers function

--- src/redux/reducers/quotes.js
+++ src/redux/reducers/quotes.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const FETCH_QUOTES = 'fetch/trump/quotes';
 
 const initialState = {
+  countQuotes: 0,
+  totalQuotes: 0,
   fetchedQoutes: [],
 };
 export const topics = (topic) => topic;
@@ -11,6 +13,8 @@ const fetchApi = 'https://www.tronalddump.io/search/quote?query=';
 
 export const fetchQuotes = (topic) => async (dispatch) => {
   const res = await axios.get(`${fetchApi}${topics(topic)}`);
+  const countQuotes = res.data.count;
+  const totalQuotes = res.data.total;
   const result = res.data._embedded.quotes;
   const fetchedQoutes = [];
   Object.entries(result).forEach(([key, value]) => {
@@ -23,14 +27,19 @@ export const fetchQuotes = (topic) => async (dispatch) => {
   });
   dispatch({
     type: FETCH_QUOTES,
-    payload: fetchedQoutes,
+    payload: { totalQuotes, countQuotes, fetchedQoutes },
   });
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_QUOTES:
-      return { ...state, fetchedQoutes: action.payload };
+      return {
+        ...state,
+        countQuotes: action.payload.countQuotes,
+        totalQuotes: action.payload.totalQuotes,
+        fetchedQoutes: action.payload.fetchedQoutes,
+      };
     default:
       return state;
   }
