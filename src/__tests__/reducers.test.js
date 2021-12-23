2b49b51390af5678bbb1a8287df1e9ab546d7eeb
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-23 23:43:22 +0200

    Add tests for fetchQuotes function

--- src/__tests__/reducers.test.js
+++ src/__tests__/reducers.test.js
@@ -1,7 +1,13 @@
-import reducer from '../redux/reducers/quotes';
+import reducer, { fetchQuotes } from '../redux/reducers/quotes';
 
 describe('Testing reducer', () => {
   test('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({ countQuotes: 0, fetchedQoutes: [], totalQuotes: 0 });
+    expect(reducer(undefined, {})).toEqual({
+      countQuotes: 0, fetchedQoutes: [], totalQuotes: 0, filteredTopics: {},
+    });
+  });
+  test('should test fetchqoutes function', () => {
+    const fetchedQoutes = fetchQuotes('OBAMA');
+    expect(fetchedQoutes).toHaveLength(1);
   });
 });
