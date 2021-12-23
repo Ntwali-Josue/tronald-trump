aa662bc92a5d9120ca52d417d06e48f62863b1d0
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-23 11:57:56 +0200

    Add tests and add snapshots

--- src/__tests__/reducers.test.js
+++ src/__tests__/reducers.test.js
@@ -2,6 +2,6 @@ import reducer from '../redux/reducers/quotes';
 
 describe('Testing reducer', () => {
   test('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({ fetchedQoutes: [] });
+    expect(reducer(undefined, {})).toEqual({ countQuotes: 0, fetchedQoutes: [], totalQuotes: 0 });
   });
 });
